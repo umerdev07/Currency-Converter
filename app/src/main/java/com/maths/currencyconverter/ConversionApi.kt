825ee0a52291ef4f42c9ed7225ae4f0423803135
M.Umer Yousaf <zonerespect2023@gmail.com>
2024-06-30 23:30:08 +0500

    Initial Commet

--- app/src/main/java/com/maths/currencyconverter/ConversionApi.kt
+++ app/src/main/java/com/maths/currencyconverter/ConversionApi.kt
@@ -1,14 +1,16 @@
-import com.maths.currencyconverter.ResponseModel
+import com.maths.currencyconverter.ConversionResponse
 import retrofit2.Call
 import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.Path
 
 interface ApiService {
-    @GET("pair")
+
+    @GET("v6/{api}/pair/{from}/{to}/{amount}")
     fun getCurrencyConversion(
-        @Query("apikey") apiKey: String,
-        @Query("from") fromCurrency: String,
-        @Query("to") toCurrency: String,
-        @Query("amount") amount: Double
-    ): Call<ResponseModel>
+        @Path("api", encoded = true) apiKey: String,
+        @Path("from") fromCurrency: String,
+        @Path("to") toCurrency: String,
+        @Path("amount") amount: Double
+    ): Call<ConversionResponse>
+
 }
