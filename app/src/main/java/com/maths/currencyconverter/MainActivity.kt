abbcda29909dc5fd4340e5b2ec4a4318afdcb98e
M.Umer Yousaf <zonerespect2023@gmail.com>
2024-07-01 20:26:46 +0500

    Initial Commet

--- app/src/main/java/com/maths/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/maths/currencyconverter/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.maths.currencyconverter
 
 import ApiService
+import android.app.AlertDialog
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.view.View
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.Window
+import android.view.WindowManager
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.maths.currencyconverter.databinding.ActivityMainBinding
@@ -13,11 +20,12 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var fromSpinner: Spinner
-    private lateinit var toSpinner: Spinner
-
+    private lateinit var fromSpinner: TextView
+    private lateinit var toSpinner: TextView
+    private lateinit var fromCurrencyTextView: TextView
+    private lateinit var toCurrencyTextView: TextView
     private lateinit var binding: ActivityMainBinding
+    private var dialog: Dialog? = null
 
     private val currencySymbols = arrayOf(
         "AFN (؋)", "ALL (L)", "DZD (د.ج)", "ARS ($)", "AUD ($)", "EUR (€)", "BDT (৳)", "EUR (€)",
@@ -30,23 +38,45 @@ class MainActivity : AppCompatActivity() {
     )
 
     private val countryList = arrayOf(
-        "Afghanistan - AFN", "Albania - ALL", "Algeria - DZD", "Argentina - ARS",
-        "Australia - AUD", "Austria - EUR", "Bangladesh - BDT", "Belgium - EUR", "Brazil - BRL",
-        "Canada - CAD", "Chile - CLP", "China - CNY", "Colombia - COP", "Denmark - DKK",
-        "Egypt - EGP", "France - EUR", "Germany - EUR", "Ghana - GHS", "Hong Kong - HKD",
-        "India - INR", "Indonesia - IDR", "Iran - IRR", "Iraq - IQD", "Italy - EUR", "Japan - JPY",
-        "Kenya - KES", "Korea South - KRW", "Malaysia - MYR", "Mexico - MXN", "Netherlands - EUR",
-        "Nigeria - NGN", "Pakistan - PKR", "Philippines - PHP", "Poland - PLN", "Qatar - QAR",
-        "Russia - RUB", "Saudi Arabia - SAR", "Singapore - SGD", "South Africa - ZAR", "Spain - EUR",
-        "Sweden - SEK", "Switzerland - CHF", "Taiwan - TWD", "Thailand - THB", "Turkey - TRY",
-        "United Arab Emirates - AED", "United Kingdom - GBP", "United States - USD", "Vietnam - VND"
+        "\uD83C\uDDE6\uD83C\uDDEB Afghanistan - AFN", "\uD83C\uDDE6\uD83C\uDDF1 Albania - ALL",
+        "\uD83C\uDDE9\uD83C\uDDFF Algeria - DZD", "\uD83C\uDDE6\uD83C\uDDF7 Argentina - ARS",
+        "\uD83C\uDDE6\uD83C\uDDFA Australia - AUD", "\uD83C\uDDE6\uD83C\uDDF9 Austria - EUR",
+        "\uD83C\uDDE7\uD83C\uDDEC Bangladesh - BDT", "\uD83C\uDDE7\uD83C\uDDEA Belgium - EUR",
+        "\uD83C\uDDE7\uD83C\uDDF7 Brazil - BRL", "\uD83C\uDDE8\uD83C\uDDE6 Canada - CAD",
+        "\uD83C\uDDE8\uD83C\uDDF1 Chile - CLP", "\uD83C\uDDE8\uD83C\uDDF3 China - CNY",
+        "\uD83C\uDDE8\uD83C\uDDF4 Colombia - COP", "\uD83C\uDDE9\uD83C\uDDF0 Denmark - DKK",
+        "\uD83C\uDDEA\uD83C\uDDEC Egypt - EGP", "\uD83C\uDDEB\uD83C\uDDF7 France - EUR",
+        "\uD83C\uDDE9\uD83C\uDDEA Germany - EUR", "\uD83C\uDDEC\uD83C\uDDED Ghana - GHS",
+        "\uD83C\uDDED\uD83C\uDDF0 Hong Kong - HKD", "\uD83C\uDDEE\uD83C\uDDF3 India - INR",
+        "\uD83C\uDDEE\uD83C\uDDE9 Indonesia - IDR", "\uD83C\uDDEE\uD83C\uDDF7 Iran - IRR",
+        "\uD83C\uDDEE\uD83C\uDDF6 Iraq - IQD", "\uD83C\uDDEE\uD83C\uDDF9 Italy - EUR",
+        "\uD83C\uDDEF\uD83C\uDDF5 Japan - JPY", "\uD83C\uDDF0\uD83C\uDDEA Kenya - KES",
+        "\uD83C\uDDF0\uD83C\uDDF7 Korea South - KRW", "\uD83C\uDDF2\uD83C\uDDFE Malaysia - MYR",
+        "\uD83C\uDDF2\uD83C\uDDFD Mexico - MXN", "\uD83C\uDDF3\uD83C\uDDF1 Netherlands - EUR",
+        "\uD83C\uDDF3\uD83C\uDDEC Nigeria - NGN", "\uD83C\uDDF5\uD83C\uDDF0 Pakistan - PKR",
+        "\uD83C\uDDF5\uD83C\uDDED Philippines - PHP", "\uD83C\uDDF5\uD83C\uDDF1 Poland - PLN",
+        "\uD83C\uDDF6\uD83C\uDDE6 Qatar - QAR", "\uD83C\uDDF7\uD83C\uDDFA Russia - RUB",
+        "\uD83C\uDDF8\uD83C\uDDE6 Saudi Arabia - SAR", "\uD83C\uDDF8\uD83C\uDDEC Singapore - SGD",
+        "\uD83C\uDDFF\uD83C\uDDE6 South Africa - ZAR", "\uD83C\uDDEA\uD83C\uDDF8 Spain - EUR",
+        "\uD83C\uDDE8\uD83C\uDDED Sweden - SEK", "\uD83C\uDDE8\uD83C\uDDEC Switzerland - CHF",
+        "\uD83C\uDDF9\uD83C\uDDFC Taiwan - TWD", "\uD83C\uDDF9\uD83C\uDDED Thailand - THB",
+        "\uD83C\uDDF9\uD83C\uDDF7 Turkey - TRY", "\uD83C\uDDE6\uD83C\uDDEA United Arab Emirates - AED",
+        "\uD83C\uDDEC\uD83C\uDDE7 United Kingdom - GBP", "\uD83C\uDDFA\uD83C\uDDF8 United States - USD",
+        "\uD83C\uDDFB\uD83C\uDDF3 Vietnam - VND"
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        supportActionBar?.hide()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        supportActionBar?.hide()
+
+        fromSpinner = binding.fromSpinner
+        toSpinner = binding.toSpinner
+        fromCurrencyTextView = binding.fromcurrency
+        toCurrencyTextView = binding.tocurrency
 
         setupSpinners()
 
@@ -57,12 +87,12 @@ class MainActivity : AppCompatActivity() {
 
     private fun convertCurrency() {
         try {
-            val fromCurrency = fromSpinner.selectedItem.toString().split(" - ")[1]
-            val toCurrency = toSpinner.selectedItem.toString().split(" - ")[1]
+            val fromCurrency = fromSpinner.text.toString().split(" - ")[1]
+            val toCurrency = toSpinner.text.toString().split(" - ")[1]
             val amountStr = binding.inputamount.text.toString()
 
             if (amountStr.isBlank()) {
-                Toast.makeText(this, "Please enter an amount", Toast.LENGTH_SHORT).show()
+                showAlertDialog("Please enter any amount")
                 return
             }
 
@@ -70,7 +100,7 @@ class MainActivity : AppCompatActivity() {
             try {
                 amount = amountStr.toDouble()
             } catch (e: NumberFormatException) {
-                Toast.makeText(this, "Invalid amount. Please enter a valid number", Toast.LENGTH_SHORT).show()
+                showAlertDialog("Invalid amount. Please enter a valid number")
                 return
             }
 
@@ -84,79 +114,127 @@ class MainActivity : AppCompatActivity() {
             val call = apiService.getCurrencyConversion("c3852155f648f6ed278ace03", fromCurrency, toCurrency, amount)
             call.enqueue(object : Callback<ConversionResponse> {
                 override fun onResponse(call: Call<ConversionResponse>, response: Response<ConversionResponse>) {
-                    try {
-                        if (response.isSuccessful) {
-                            val data = response.body()
-                            if (data != null) {
-                                val convertedAmount = data.conversionResult
-                                binding.convertedcurrency.text = convertedAmount.toString()
-                            } else {
-                                Toast.makeText(this@MainActivity, "Response body is null", Toast.LENGTH_SHORT).show()
-                            }
+                    if (response.isSuccessful) {
+                        val data = response.body()
+                        if (data != null) {
+                            val convertedAmount = data.conversionResult
+                            binding.convertedcurrency.text = convertedAmount.toString()
                         } else {
-                            Toast.makeText(
-                                this@MainActivity, "Failed to fetch data: ${response.code()} - ${response.message()}", Toast.LENGTH_SHORT).show()
+                            showAlertDialog("Response body is null")
                         }
-                    } catch (e: Exception) {
-                        Toast.makeText(this@MainActivity, "Error processing response: ${e.message}", Toast.LENGTH_SHORT).show()
+                    } else {
+                       showAlertDialog("Failed to get conversion rate: ${response.message()}")
                     }
                 }
 
                 override fun onFailure(call: Call<ConversionResponse>, t: Throwable) {
-                    Toast.makeText(this@MainActivity, "Error: ${t.message}", Toast.LENGTH_SHORT).show()
+                    showAlertDialog("Error: Failed to connect server or Internet Connection")
                 }
             })
         } catch (e: Exception) {
-            Toast.makeText(this, "An unexpected error occurred: ${e.message}", Toast.LENGTH_SHORT).show()
+            showAlertDialog("Error: Connect your Internet}")
         }
     }
 
     private fun setupSpinners() {
-        fromSpinner = binding.fromSpinner
-        toSpinner = binding.toSpinner
+        fromSpinner.setOnClickListener {
+            showFromSpinnerDialog()
+        }
+        toSpinner.setOnClickListener {
+            showToSpinnerDialog()
+        }
+    }
 
-        val defaultFromIndex = countryList.indexOf("Pakistan - PKR")
-        val defaultToIndex = countryList.indexOf("United States - USD")
+    private fun showFromSpinnerDialog() {
+        dialog = Dialog(this@MainActivity)
+        dialog!!.setContentView(R.layout.dialog_searchable_spinner)
+        dialog!!.window!!.setLayout(950, 1400)
+        dialog!!.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog!!.show()
 
-        fromSpinner.setSelection(defaultFromIndex)
-        toSpinner.setSelection(defaultToIndex)
+        val editText = dialog!!.findViewById<EditText>(R.id.search)
+        val listView = dialog!!.findViewById<ListView>(R.id.list_view)
 
-        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, countryList)
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        fromSpinner.adapter = adapter
-        toSpinner.adapter = adapter
+        val adapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_list_item_1, countryList)
+        listView.adapter = adapter
 
-        fromSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                updateCurrencySymbol(binding.fromcurrency, position)
-                validateCurrencySelection()
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                adapter.filter.filter(s)
             }
 
-            override fun onNothingSelected(parent: AdapterView<*>?) {}
-        }
+            override fun afterTextChanged(s: Editable) {}
+        })
 
-        toSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                updateCurrencySymbol(binding.tocurrency, position)
-                validateCurrencySelection()
+        listView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            val selectedCountry = adapter.getItem(position)!!
+            val selectedCurrency = currencySymbols[countryList.indexOf(selectedCountry)]
+
+            fromCurrencyTextView.text = selectedCurrency.split(" ")[0]
+
+            val tocountry = toSpinner.text.toString()
+            if (selectedCountry == tocountry) {
+                showAlertDialog("Both countries must be different")
+            } else {
+                fromSpinner.text = selectedCountry
+                dialog!!.dismiss()
+            }
+        }
+    }
+
+    private fun showToSpinnerDialog() {
+        dialog = Dialog(this@MainActivity)
+        dialog!!.setContentView(R.layout.dialog_searchable_spinner)
+        dialog!!.window!!.setLayout(950, 1400)
+        dialog!!.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog!!.show()
+
+        val editText = dialog!!.findViewById<EditText>(R.id.search)
+        val listView = dialog!!.findViewById<ListView>(R.id.list_view)
+
+        val adapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_list_item_1, countryList)
+        listView.adapter = adapter
+
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                adapter.filter.filter(s)
             }
 
-            override fun onNothingSelected(parent: AdapterView<*>?) {}
+            override fun afterTextChanged(s: Editable) {}
+        })
+
+        listView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            val selectedCountry = adapter.getItem(position)!!
+            val selectedCurrency = currencySymbols[countryList.indexOf(selectedCountry)]
+            toCurrencyTextView.text = selectedCurrency.split(" ")[0]
+
+            val fromCountry = fromSpinner.text.toString()
+            if (selectedCountry == fromCountry) {
+                showAlertDialog("Both countries must be different")
+            } else {
+                toSpinner.text = selectedCountry
+                dialog!!.dismiss()
+            }
         }
     }
 
-    private fun updateCurrencySymbol(textView: TextView, position: Int) {
-        if (position >= 0 && position < currencySymbols.size) {
-            val symbol = currencySymbols[position]
-            textView.text = symbol
-        }
+    private fun showAlertDialog(message: String) {
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage(message)
+
+            .setCancelable(true)
+            .setPositiveButton("OK") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .setNegativeButton("No") { dialog, _ ->
+                dialog.dismiss()
+            }
+        val alert = builder.create()
+        alert.show()
     }
 
-    private fun validateCurrencySelection() {
-        val fromIndex = fromSpinner.selectedItemPosition
-        val toIndex = toSpinner.selectedItemPosition
-        if (fromIndex == toIndex) {
-            Toast.makeText(this, "Currency cannot be the same", Toast.LENGTH_SHORT).show()
-        }
-    }
 }
